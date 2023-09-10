ddd81d5accb2b29e44a1d0a09ef1fc681d1fb235
Eduardo Bustamante Valencia <eduardo@MacBook-Pro-de-Eduardo-4.local>
2023-09-10 01:49:08 -0500

    fix to loading

--- src/pages/attendee/scan.tsx
+++ src/pages/attendee/scan.tsx
@@ -1,13 +1,12 @@
-import * as React from 'react'
-import { useState } from 'react'
-import { QrReader } from 'react-qr-reader'
-import { Layout } from '../../components/Layout'
-import { navigate } from 'gatsby'
+import * as React from "react";
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
+import { Layout } from "../../components/Layout";
+import { navigate } from "gatsby";
 
 const Scan = () => {
-  const [selected, setSelected] = useState('environment')
-  const [startScan, setStartScan] = useState(false)
-  const [loadingScan, setLoadingScan] = useState(false)
+  const [selected, setSelected] = useState("environment");
+  const [startScan, setStartScan] = useState(false);
 
   return (
     <Layout>
@@ -18,7 +17,7 @@ const Scan = () => {
               className="px-5 py-2 border rounded-md m-auto block hover:bg-black hover:text-white mb-6"
               onClick={() => setStartScan(!startScan)}
             >
-              {startScan ? 'Desactivar Camara' : 'Activar Camara'}
+              {startScan ? "Desactivar Camara" : "Activar Camara"}
             </button>
             {startScan && (
               <>
@@ -26,29 +25,28 @@ const Scan = () => {
                   className="block m-auto"
                   onChange={(e) => setSelected(e.target.value)}
                 >
-                  <option value={'environment'}>Back Camera</option>
-                  <option value={'user'}>Front Camera</option>
+                  <option value={"environment"}>Back Camera</option>
+                  <option value={"user"}>Front Camera</option>
                 </select>
                 <QrReader
                   constraints={{ facingMode: selected }}
                   onResult={(result, error) => {
                     if (result) {
-                      const arr = result?.getText().split('/')
-                      navigate(`/attendee/${arr[arr.length - 1]}`)
+                      const arr = result?.getText().split("/");
+                      navigate(`/attendee/${arr[arr.length - 1]}`);
                     }
                   }}
                   className="sm:w-4/5 py-3 m-auto lg:w-2/4"
                 />
               </>
             )}
-            {loadingScan && <p>Loading</p>}
           </div>
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default Scan
+export default Scan;
 
-export { Head } from '../index'
+export { Head } from "../index";

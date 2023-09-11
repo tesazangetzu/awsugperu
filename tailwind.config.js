cd3592a84cef32e2718f0b7e5c27596154346a04
Eduardo Bustamante Valencia <eduardo@MacBook-Pro-de-Eduardo-4.local>
2023-09-11 18:04:47 -0500

    change image banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}'
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'header-custom': "url('../images/back-header.png')",
-        'reward-custom': "url('../images/reward.jpg')"
+        "header-custom": "url('../images/back-header.png')",
+        "reward-custom": "url('../images/reward.jpg')",
       },
       backgroundColor: {
-        'white-trans-6': '#ffffffd1'
-      }
-    }
+        "white-trans-6": "#ffffffd1",
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
